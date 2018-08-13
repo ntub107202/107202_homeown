e5d4fdef12309a804e34d07ac181af9a380276f9
Kevin Yu <s920613a@gmail.com>
2018-08-13 17:34:03 +0800

    頁面連結
    民宿資訊到新增民宿資訊

---
+++ app/src/main/java/ntub107202/hostel/Hotelview_Addhotel.java
@@ -0,0 +1,12 @@
+package ntub107202.hostel;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Hotelview_Addhotel extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hotelview_addhotel);
+    }
+}
--- app/src/main/java/ntub107202/hostel/Setting_Hotelinfo.java
+++ app/src/main/java/ntub107202/hostel/Setting_Hotelinfo.java
@@ -1,7 +1,11 @@
 package ntub107202.hostel;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class Setting_Hotelinfo extends AppCompatActivity {
@@ -9,6 +13,15 @@ public class Setting_Hotelinfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setting_hotelinfo);
+        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab_go_to_add_hotel) ;
+        fab.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Setting_Hotelinfo.this,Hotelview_Addhotel.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }

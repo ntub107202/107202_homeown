9738c339cd562ba7b78ef19fe69d78513076f7cf
Kevin Yu <s920613a@gmail.com>
2018-08-20 22:46:15 +0800

    新增頁面
    新增履歷介面獨立於Fragment3

--- app/src/main/java/ntub107202/hostel/NavigationActivity.java
+++ app/src/main/java/ntub107202/hostel/NavigationActivity.java
@@ -7,7 +7,6 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.TextView;
 
 public class NavigationActivity extends AppCompatActivity {
@@ -15,8 +14,9 @@ public class NavigationActivity extends AppCompatActivity {
     private TextView mTextMessage;
     private Fragment_Humansearch Fragment1HumanSearch;
     private Fragment_Jobview Fragment2JobView;
-    private Fragment_Calenderview Fragment3CalenderView;
-    private Fragment_Bosssetting Fragment4BossSetting;
+    private Fragment_Inbox Fragment3Inbox;
+    private Fragment_Calenderview Fragment4CalenderView;
+    private Fragment_Bosssetting Fragment5BossSetting;
     BottomNavigationView navigation;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -33,26 +33,16 @@ public class NavigationActivity extends AppCompatActivity {
                     return true;
                 case R.id.navigation_publish:
                     getWorksheet.getjobJSON();
-//                    Fragment2JobView.setrow5(getWorksheet.getRow5(0));
-//                    Fragment2JobView.setrow6(getWorksheet.getRow6(0));
-//                    Fragment2JobView.setrow7(getWorksheet.getRow7(0));
-//                    Fragment2JobView.setrow8(getWorksheet.getRow8(0));
-//                    if(Fragment2JobView.textView1.getText() != ""){
-//                        Fragment2JobView.textView0.setVisibility(View.GONE);
-//                    }
                     mTextMessage.setText("");
                     showNav(R.id.navigation_publish);
                     return true;
+                case R.id.navigation_inbox:
+                    getWorksheet.getjobJSON();
+                    mTextMessage.setText("");
+                    showNav(R.id.navigation_inbox);
+                    return true;
                 case R.id.navigation_work:
                     getWorksheet.getjobJSON();
-//                    Fragment3CalenderView.setrow9(getWorksheet.getRow9(0));
-//                    Fragment3CalenderView.setrow10(getWorksheet.getRow10(0));
-//                    Fragment3CalenderView.setrow11(getWorksheet.getRow11(0));
-//                    Fragment3CalenderView.setrow12(getWorksheet.getRow12(0));
-//                    if(Fragment3CalenderView.textView31.getText() != ""){
-//                        Fragment3CalenderView.textView30.setVisibility(View.GONE);
-//                    }
-//                    mTextMessage.setText("");
                     mTextMessage.setText("");
                     showNav(R.id.navigation_work);
                     return true;
@@ -92,29 +82,36 @@ public class NavigationActivity extends AppCompatActivity {
         FragmentTransaction beginTransaction = getFragmentManager().beginTransaction();
         switch (id) {
             case 1:
-                beginTransaction.hide(Fragment2JobView).hide(Fragment3CalenderView).hide(Fragment4BossSetting);
+                beginTransaction.hide(Fragment2JobView).hide(Fragment3Inbox).hide(Fragment4CalenderView).hide(Fragment5BossSetting);
                 beginTransaction.show(Fragment1HumanSearch);
                 beginTransaction.addToBackStack(null);
                 beginTransaction.commit();
                 navigation.getMenu().getItem(0).setChecked(true);
                 break;
             case 2:
-                beginTransaction.hide(Fragment1HumanSearch).hide(Fragment3CalenderView).hide(Fragment4BossSetting);
+                beginTransaction.hide(Fragment1HumanSearch).hide(Fragment3Inbox).hide(Fragment4CalenderView).hide(Fragment5BossSetting);
                 beginTransaction.show(Fragment2JobView);
                 beginTransaction.addToBackStack(null);
                 beginTransaction.commit();
                 navigation.getMenu().getItem(1).setChecked(true);
                 break;
             case 3:
-                beginTransaction.hide(Fragment2JobView).hide(Fragment1HumanSearch).hide(Fragment4BossSetting);
-                beginTransaction.show(Fragment3CalenderView);
+                beginTransaction.hide(Fragment2JobView).hide(Fragment1HumanSearch).hide(Fragment5BossSetting).hide(Fragment4CalenderView);
+                beginTransaction.show(Fragment3Inbox);
                 beginTransaction.addToBackStack(null);
                 beginTransaction.commit();
                 navigation.getMenu().getItem(2).setChecked(true);
                 break;
             case 4:
-                beginTransaction.hide(Fragment1HumanSearch).hide(Fragment2JobView).hide(Fragment3CalenderView);
-                beginTransaction.show(Fragment4BossSetting);
+                beginTransaction.hide(Fragment2JobView).hide(Fragment3Inbox).hide(Fragment1HumanSearch).hide(Fragment5BossSetting);
+                beginTransaction.show(Fragment4CalenderView);
+                beginTransaction.addToBackStack(null);
+                beginTransaction.commit();
+                navigation.getMenu().getItem(2).setChecked(true);
+                break;
+            case 5:
+                beginTransaction.hide(Fragment1HumanSearch).hide(Fragment2JobView).hide(Fragment3Inbox).hide(Fragment4CalenderView);
+                beginTransaction.show(Fragment5BossSetting);
                 beginTransaction.addToBackStack(null);
                 beginTransaction.commit();
                 mTextMessage.setText("");
@@ -142,11 +139,12 @@ public class NavigationActivity extends AppCompatActivity {
     private void init() {
         Fragment1HumanSearch = new Fragment_Humansearch();
         Fragment2JobView = new Fragment_Jobview();
-        Fragment3CalenderView = new Fragment_Calenderview();
-        Fragment4BossSetting = new Fragment_Bosssetting();
+        Fragment3Inbox = new Fragment_Inbox();
+        Fragment4CalenderView = new Fragment_Calenderview();
+        Fragment5BossSetting = new Fragment_Bosssetting();
         FragmentTransaction beginTransaction = getFragmentManager().beginTransaction();
-        beginTransaction.add(R.id.content, Fragment1HumanSearch).add(R.id.content, Fragment2JobView).add(R.id.content, Fragment3CalenderView).add(R.id.content, Fragment4BossSetting);//开启一个事务将fragment动态加载到组件
-        beginTransaction.hide(Fragment1HumanSearch).hide(Fragment2JobView).hide(Fragment3CalenderView).hide(Fragment4BossSetting);//隐藏fragment
+        beginTransaction.add(R.id.content, Fragment1HumanSearch).add(R.id.content, Fragment2JobView).add(R.id.content, Fragment3Inbox).add(R.id.content, Fragment4CalenderView).add(R.id.content, Fragment5BossSetting);//开启一个事务将fragment动态加载到组件
+        beginTransaction.hide(Fragment1HumanSearch).hide(Fragment2JobView).hide(Fragment3Inbox).hide(Fragment4CalenderView).hide(Fragment5BossSetting);//隐藏fragment
         beginTransaction.addToBackStack(null);//返回到上一个显示的fragment
         beginTransaction.commit();//每一个事务最后操作必须是commit（），否则看不见效果
         showNav(R.id.navigation_people);
@@ -157,26 +155,32 @@ public class NavigationActivity extends AppCompatActivity {
         FragmentTransaction beginTransaction = getFragmentManager().beginTransaction();
         switch (navid) {
             case R.id.navigation_people:
-                beginTransaction.hide(Fragment2JobView).hide(Fragment3CalenderView).hide(Fragment4BossSetting);
+                beginTransaction.hide(Fragment2JobView).hide(Fragment3Inbox).hide(Fragment4CalenderView).hide(Fragment5BossSetting);
                 beginTransaction.show(Fragment1HumanSearch);
                 beginTransaction.addToBackStack(null);
                 beginTransaction.commit();
                 break;
             case R.id.navigation_publish:
-                beginTransaction.hide(Fragment1HumanSearch).hide(Fragment3CalenderView).hide(Fragment4BossSetting);
+                beginTransaction.hide(Fragment1HumanSearch).hide(Fragment3Inbox).hide(Fragment4CalenderView).hide(Fragment5BossSetting);
                 beginTransaction.show(Fragment2JobView);
                 beginTransaction.addToBackStack(null);
                 beginTransaction.commit();
                 break;
+            case R.id.navigation_inbox:
+                beginTransaction.hide(Fragment1HumanSearch).hide(Fragment2JobView).hide(Fragment4CalenderView).hide(Fragment5BossSetting);
+                beginTransaction.show(Fragment3Inbox);
+                beginTransaction.addToBackStack(null);
+                beginTransaction.commit();
+                break;
             case R.id.navigation_work:
-                beginTransaction.hide(Fragment2JobView).hide(Fragment1HumanSearch).hide(Fragment4BossSetting);
-                beginTransaction.show(Fragment3CalenderView);
+                beginTransaction.hide(Fragment2JobView).hide(Fragment3Inbox).hide(Fragment1HumanSearch).hide(Fragment5BossSetting);
+                beginTransaction.show(Fragment4CalenderView);
                 beginTransaction.addToBackStack(null);
                 beginTransaction.commit();
                 break;
             case R.id.navigation_page:
-                beginTransaction.hide(Fragment1HumanSearch).hide(Fragment2JobView).hide(Fragment3CalenderView);
-                beginTransaction.show(Fragment4BossSetting);
+                beginTransaction.hide(Fragment1HumanSearch).hide(Fragment2JobView).hide(Fragment3Inbox).hide(Fragment4CalenderView);
+                beginTransaction.show(Fragment5BossSetting);
                 beginTransaction.addToBackStack(null);
                 beginTransaction.commit();
                 break;
